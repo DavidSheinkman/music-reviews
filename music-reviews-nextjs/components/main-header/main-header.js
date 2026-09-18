f4bbef9d3a3ca414d0dccffabda0e0b4b3f6950b
DavidSheinkman <Poilkjmnb!davidsheinkman7>
2026-09-18 19:54:50 +0200

    next.js Image + header

---
+++ music-reviews-nextjs/components/main-header/main-header.js
@@ -0,0 +1,30 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import logoImg from '@/assets/logo.png'
+import classes from './main-header.module.css';
+import MainHeaderBackground from './main-header-background';
+
+export default function MainHeader() {
+  return (
+
+    <>
+      <MainHeaderBackground />
+      <header className={classes.header}>
+        <Link href="/" className={classes.logo}>
+
+          <Image src={logoImg} alt="Logo" priority />
+          Music Reviews
+        </Link>
+
+        <nav className={classes.nav}>
+          <ul>
+            <li><Link href="/reviews">Browse Reviews</Link></li>
+            <li><Link href="/community">Community</Link></li>
+          </ul>
+        </nav>
+
+
+      </header>
+    </>
+  );
+}
