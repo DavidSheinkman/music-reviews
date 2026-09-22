cd2a0ce34ba213be815436117e734888a00ed489
DavidSheinkman <Poilkjmnb!davidsheinkman7>
2026-09-22 17:22:54 +0200

    ImageSlideshow

---
+++ music-reviews-nextjs/components/images/image-slideshow.js
@@ -0,0 +1,50 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+import Image from 'next/image';
+
+import AphexTwin from '@/assets/AphexTwin.avif';
+import FatimaYamaha from '@/assets/FatimaYamaha.avif';
+import FloatingPoints from '@/assets/FloatingPoints.avif';
+import Gas from '@/assets/Gas.avif';
+import OctaveOne from '@/assets/OctaveOne.avif';
+import RivalConsoles from '@/assets/RivalConsoles.avif';
+import Shx from '@/assets/Shx.avif';
+import classes from './image-slideshow.module.css';
+
+const images = [
+  { image: AphexTwin, alt: 'Aphex Twin album cover' },
+  { image: FloatingPoints, alt: 'Floating Points album cover' },
+  { image: Gas, alt: 'Gas album cover' },
+  { image: OctaveOne, alt: 'Octave One album cover' },
+  { image: RivalConsoles, alt: 'Rival Consoles album cover' },
+  { image: Shx, alt: 'SHXCXCHCXSH album cover' },
+  { image: FatimaYamaha, alt: 'Fatima Yamaha album cover' },
+];
+
+export default function ImageSlideshow() {
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentImageIndex((prevIndex) =>
+        prevIndex < images.length - 1 ? prevIndex + 1 : 0
+      );
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <div className={classes.slideshow}>
+      {images.map((image, index) => (
+        <Image
+          key={index}
+          src={image.image}
+          className={index === currentImageIndex ? classes.active : ''}
+          alt={image.alt}
+        />
+      ))}
+    </div>
+  );
+}
