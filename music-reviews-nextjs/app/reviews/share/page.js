ab864d16c1bf289b27a2fde481f63d33e77a2f58
DavidSheinkman <Poilkjmnb!davidsheinkman7>
2026-09-24 20:42:00 +0200

    share review page + image picker

--- music-reviews-nextjs/app/reviews/share/page.js
+++ music-reviews-nextjs/app/reviews/share/page.js
@@ -1,7 +1,51 @@
+import classes from './page.module.css';
+
+import ImagePicker from '@/components/reviews/image-picker';
+
 export default function ShareReviewPage() {
-return (
-
-    <h1>Share Your Music Review</h1>    
-);
-
+  return (
+    <>
+      <header className={classes.header}>
+        <h1>
+          Share your <span className={classes.highlight}>favorite music album review</span>
+        </h1>
+        <p>Or any other music review you feel needs sharing!</p>
+      </header>
+      <main className={classes.main}>
+        <form className={classes.form}>
+          <div className={classes.row}>
+            <p>
+              <label htmlFor="name">Your name</label>
+              <input type="text" id="name" name="name" required />
+            </p>
+            <p>
+              <label htmlFor="email">Your email</label>
+              <input type="email" id="email" name="email" required />
+            </p>
+          </div>
+          <p>
+            <label htmlFor="title">Title</label>
+            <input type="text" id="title" name="title" required />
+          </p>
+          <p>
+            <label htmlFor="summary">Short Summary</label>
+            <input type="text" id="summary" name="summary" required />
+          </p>
+          <p>
+            <label htmlFor="instructions">Content</label>
+            <textarea
+              id="instructions"
+              name="instructions"
+              rows="10"
+              required
+            ></textarea>
+          </p>
+          <ImagePicker />
+          <p className={classes.actions}>
+            <button type="submit">Share Review</button>
+          </p>
+        </form>
+      </main>
+    </>
+  );
 }
